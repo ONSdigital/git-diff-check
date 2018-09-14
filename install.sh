f73d39256bcfe8ca2b28ebb54b1a067b2dfd4621
J Gregory <john@johngregory.me.uk>
2018-09-14 14:53:39 +0100

    add version to build and check during install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 repo='ONSdigital/git-diff-check'
+binary='pre-commit'
 
 get_latest_release() { # From https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
   curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
@@ -8,15 +9,18 @@ get_latest_release() { # From https://gist.github.com/lukechilds/a83e1d7127b78fe
     sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
 }
 
+# Fetch the most recent release number from github
+release_version=$(get_latest_release ${repo})
+binary="${binary}_${release_version}"
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  binary='pre-commit_darwin-amd64'
+  binary="${binary}_darwin-amd64"
   target=${HOME}/.githooks
 else
   echo "OS '${OSTYPE}' not currently supported by installer - please refer to manual instructions in the README."
   exit 0
 fi
 
-release_version=$(get_latest_release ${repo})
 release="https://github.com/${repo}/releases/download/${release_version}/${binary}"
 
 # Create the target location if it doesn't already exist
@@ -28,9 +32,10 @@ release="https://github.com/${repo}/releases/download/${release_version}/${binar
 
 # Check if we're up to date
 echo "Check for previous versions ..."
-[ -f ${target}/pre-commit.version ] &&
+[ -f ${target}/pre-commit ] &&
   {
-    existing=$(cat ${target}/pre-commit.version)
+    existing="$(${target}/pre-commit --version)"
+    # existing=$(cat ${target}/pre-commit.version)
     echo "-- found existing version ${existing}"
     [ "${existing}" = "$release_version" ] &&
       {
@@ -43,14 +48,19 @@ echo "Check for previous versions ..."
 # Fetch the tool
 echo "Fetching Git Diff precommit hook ${release_version} ..."
 echo "-- from ${release} ..."
-curl -L --progress-bar ${release} -o "${target}/pre-commit"
+curl -L --progress-bar -f ${release} -o "${target}/pre-commit"
+
+# Check whether cURL was successful
+[ $? != 0 ] &&
+  {
+    echo "Oops, something went wrong. Couldn't fetch!"
+    exit 1
+  }
+
 chmod +x "${target}/pre-commit"
 
 # Update the git config
 echo "Updating git config ..."
 git config --global core.hooksPath ${target}
-
-# Store the installed version
-echo ${release_version} > ${target}/pre-commit.version
 
 echo "Add done!"
